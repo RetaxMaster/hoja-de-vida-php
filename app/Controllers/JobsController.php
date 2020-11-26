4f5a643602e65432233950df74b483310a63667f
RetaxMaster <retaxmaster@hotmail.com>
2020-11-25 23:17:13 -0600

    Adicion de las validaciones

--- app/Controllers/JobsController.php
+++ app/Controllers/JobsController.php
@@ -3,23 +3,40 @@
 namespace App\Controllers;
 
 use App\Models\Job;
+use Respect\Validation\Validator as v;
 
 class JobsController extends BaseController {
 
     // El request viene desde nuestro FrontController public/index.php
     public function getAddJobAction($request) {
+
+        $responseMessage = null;
         
         if ($request->getMethod() == "POST") {
-
+            
             $postData = $request->getParsedBody();
-            $job = new Job();
-            $job->title = $postData["title"];
-            $job->description = $postData["description"];
-            $job->save();
+
+            $jobValidator = v::key("title", v::stringType()->notEmpty())
+                            ->key("description", v::stringType()->notEmpty());
+
+            try {
+
+                $jobValidator->assert($postData);
+                $job = new Job();
+                $job->title = $postData["title"];
+                $job->description = $postData["description"];
+                $job->save();
+
+                $responseMessage = "Saved";
+
+            } catch (\Exception $e) {
+                $responseMessage = $e->getMessage();
+            }
+     
 
         }
 
-        return $this->renderHTML("addJob.twig");
+        return $this->renderHTML("addJob.twig", compact("responseMessage"));
 
     }
 
